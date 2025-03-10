e7e528c20d6505408e62815ce9f866e0932a9423
Diego Pacheco Flores <diegopatcheco@gmail.com>
2025-03-10 17:51:15 -0600

    remote_driver
    adding remote driver

--- src/test/java/utilities/DriverManager.java
+++ src/test/java/utilities/DriverManager.java
@@ -1,15 +1,20 @@
 package utilities;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DriverManager {
-    private final boolean runServer = System.getenv("JOB_NAME") != null;
+    private final boolean runServer = System.getenv("Run Tests and Generate Allure Report") != null;
 
     public void buildDriver() {
         if (runServer) {
@@ -20,6 +25,53 @@ public class DriverManager {
     }
 
     private void buildRemoteDriver() {
+        final var browserProperty = System.getProperty("browser", "CHROME").toUpperCase();
+        final var headlessMode = System.getProperty("headless") != null;
+
+        Logs.debug("Inicializando driver remoto: %s", browserProperty);
+
+        final var capabilities = switch (Browser.valueOf(browserProperty)) {
+            case CHROME -> {
+                var options = new ChromeOptions();
+                if (headlessMode) {
+                    options.addArguments("--headless=new");
+                }
+                yield options;
+            }
+            case EDGE -> {
+                var options = new EdgeOptions();
+                if (headlessMode) {
+                    options.addArguments("--headless=new");
+                }
+                yield options;
+            }
+            case FIREFOX -> {
+                var options = new FirefoxOptions();
+                if (headlessMode) {
+                    options.addArguments("--headless");
+                }
+                yield options;
+            }
+            case SAFARI -> new SafariDriver();
+        };
+
+        try {
+            var remoteUrl = new URL(System.getProperty("remoteUrl", "http://localhost:4444/wd/hub"));
+            var driver = new RemoteWebDriver(remoteUrl, (Capabilities) capabilities);
+
+            Logs.debug("Maximizando la pantalla en el entorno remoto");
+            driver.manage().window().maximize();
+
+            Logs.debug("Borrando las cookies en el entorno remoto");
+            driver.manage().deleteAllCookies();
+
+            Logs.debug("Asignando driver remoto al WebDriver provider");
+            new WebDriverProvider().set(driver);
+
+        } catch (MalformedURLException e) {
+            Logs.error("URL remota no válida para Selenium Grid", e);
+            throw new RuntimeException(e);
+        }
     }
 
     private void buildLocalDriver() {
