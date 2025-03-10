a541e1c50372a08c87be0764e78962825917e45a
Diego Pacheco Flores <diegopatcheco@gmail.com>
2025-03-10 17:57:32 -0600

    Update CartPage.java

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -2,13 +2,20 @@ package pages;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.BasePage;
 
+import java.time.Duration;
+import java.util.List;
+
 public class CartPage extends BasePage {
-    private final By emptyCartLabel = By.className("text-center");
+    private final By emptyCartLabel = By.id("empty_cart");
     private final By shoppingCartTitle = By.className("active");
     private final By buyProductsLink = By.cssSelector("a[href='/products']");
     private final By itemsList = By.cssSelector("tr[id]");
+    private List<WebElement> itemsLocatorList;
 
     @Override
     @Step("Waiting to cart page to load")
@@ -28,7 +35,17 @@ public class CartPage extends BasePage {
 
     @Step("Verify cart is empty")
     public void verifyCartEmpty() {
-        softAssert.assertTrue(find(emptyCartLabel).isDisplayed());
+        final var wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        itemsLocatorList = findAll(itemsList);
+
+        if (!itemsLocatorList.isEmpty()) {
+            for (var i = 1; i < itemsLocatorList.size() + 1; i++) {
+                find(removeItemButtonDynamicLocator(i)).click();
+            }
+        }
+
+        final var label = wait.until(ExpectedConditions.visibilityOfElementLocated(emptyCartLabel));
+        softAssert.assertTrue(label.isDisplayed());
         softAssert.assertAll();
     }
 
@@ -36,5 +53,9 @@ public class CartPage extends BasePage {
     public void verifyTotalCartItems(int expectedTotalItems) {
         softAssert.assertEquals(findAll(itemsList).size(), expectedTotalItems);
         softAssert.assertAll();
+    }
+
+    private By removeItemButtonDynamicLocator(int itemId) {
+        return By.cssSelector(String.format("a[data-product-id='%d']", itemId));
     }
 }
